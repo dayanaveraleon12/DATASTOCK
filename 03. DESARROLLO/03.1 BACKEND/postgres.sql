4909eaced8f17de12af8dc1704450e4e1620141d
Juan David Sanchez <juansanchezsan077@gmail.com>
2026-09-21 21:47:04 +0000

    chore: se agrega tabla 16

--- 03. DESARROLLO/03.1 BACKEND/postgres.sql
+++ 03. DESARROLLO/03.1 BACKEND/postgres.sql
@@ -1,1 +1,15 @@
 create extension if not exists pgcrypto;
+
+-- tabla 16 juan david
+create table producto_sitio(
+    id integer not null,
+    id_producto integer not null,
+    id_sitio integer not null,
+    id_lote integer not null,
+    cantidad_sitio integer,
+    constraint pk_producto_sitio primary key(id),
+    constraint fk_producto__producto_sitio FOREIGN key (id_producto) references producto(id),
+    constraint fk_sitio__producto_sitio FOREIGN key (id_sitio) references sitio (id),
+    constraint fk_lote__producto_sitio FOREIGN key (id_lote) references lote(id),
+    constraint uk_producto_sitio UNIQUE(id_producto, id_sitio, id_lote)
+);
